f454bd074233cef9c133aa17eed4242f7e44894f
silviaplabon <silviaplabon@gmail.com>
2021-01-29 15:01:00 +0600

    sil

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const phone1 = 1239, phone2 = 59;
 document.getElementById('phone1plus').addEventListener('click', function () {
-   getDetails('phone1price', phone1, 'sil1', 1, 'subtotal', 'taxbd', 'subtotals')
+   getDetails('phone1price', phone1, 'sil1', 1)
 })
 document.getElementById('phone1minus').addEventListener('click', function () {
-   getDetails('phone1price', -1 * phone1, 'sil1', -1, 'subtotal', 'taxbd', 'subtotals')
+   getDetails('phone1price', -1 * phone1, 'sil1', -1)
 })
 document.getElementById('phone2plus').addEventListener('click', function () {
-   getDetails('phone2price', phone2, 'sil2', 1, 'subtotal', 'taxbd', 'subtotals')
+   getDetails('phone2price', phone2, 'sil2', 1)
 })
 document.getElementById('phone2minus').addEventListener('click', function () {
-   getDetails('phone2price', -1 * phone2, 'sil2', -1, 'subtotal', 'taxbd', 'subtotals')
+   getDetails('phone2price', -1 * phone2, 'sil2', -1)
 })
-function getDetails(phoneid, phoneidprice, quantityid, quantitysize, subtotalid, taxid, subtotalsid) {
+function getDetails(phoneid, phoneidprice, quantityid, quantitysize) {
    const quantity = document.getElementById(quantityid).value; //quantitydetails::
    const quantitytotal = parseInt(quantity) + quantitysize;
    if (quantitytotal >= 0) {
@@ -19,13 +19,13 @@ function getDetails(phoneid, phoneidprice, quantityid, quantitysize, subtotalid,
       const current = parseFloat(document.getElementById(phoneid).innerText); // price update working details
       const totalprice = current + phoneidprice;
       document.getElementById(phoneid).innerText = totalprice; //price update details
-      const values = parseFloat(document.getElementById(subtotalid).innerText); //subtotal
+      const values = parseFloat(document.getElementById('subtotal').innerText); //subtotal
       const total = (values + phoneidprice);
-      document.getElementById(subtotalid).innerText = total;   //subtotal end
+      document.getElementById('subtotal').innerText = total;   //subtotal end
       const tax = (total * 0.005).toFixed(2);
-      document.getElementById(taxid).innerText = tax;
+      document.getElementById('taxbd').innerText = tax;
       const bestanswer = parseFloat(total) + parseFloat(tax);
-      document.getElementById(subtotalsid).innerText = bestanswer;
+      document.getElementById('subtotals').innerText = bestanswer;
    }
    if (quantitytotal < 0) {
       document.getElementById(quantityid).value = 0;
